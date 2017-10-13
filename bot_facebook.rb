043357ab343127b3537fa992d333e1db673c753d
tropikbrice <blabejof@gmail.com>
2017-10-13 19:04:49 +0200

    tentative de gerer le see more : end of world

--- bot_facebook.rb
+++ bot_facebook.rb
@@ -41,12 +41,60 @@ class Facebook_scrap
 		@browser.button(:id,"loginbutton").click
 	end
 
+	#on definit une fct qui va sur les groupes
+	def fb_grp_scrap()
+		@browser.goto @page_url
+
+		#on doit cliquer sur "see more" afin de charger tte la page (ds mon FB, il se gere automatiquement)
+		#on va cliquer sur l element TANT QUE il est present (marche aussi pour visible, exists)
+
+		#ya une popup qui arrive et qui m'emmerde, on va essayer de la degager:
+		#browser.element(:xpath => "//h3[@class='_52c9']")
+		#@browser.execute_script("window.scrollBy(0,20000)")
+		while true # ou visible? exists?
+
+			#le begin afin de gerer l erreur sur le temps ecoule
+			begin
+				#possibilite de virer la popup facebook
+				puts "popup s affiche" if browser.alert.present?
+				binding.pry
+				# @browser.alert.ok.click if browser.alert.present?
+				# # Remove the function
+				# @browser.execute_script("window.onbeforeunload = null")
+				# # Then trigger the action that leaves the page
+				# @browser.link.click
+
+				popup_fb= @browser.element(:xpath => "//a[@class='layerCancel _4jy0 _4jy3 _517h _51sy _42ft']")
+				if popup_fb.present?
+					puts "popup presente"
+					popup_fb.click
+				end
+
+
+				link_see_more = @browser.element(:xpath => "//a[@class='pam uiBoxLightblue uiMorePagerPrimary']")
+				link_see_more.click
+				#sleep(3)				
+
+				#condition de sortie de la boucle infinie : il n'y a plus le bouton see more
+				if link_see_more.present? == false
+					puts "plus de see more on sort de l infinie"
+					break
+				end
+			rescue Watir::Exception::UnknownObjectException, Watir::Wait::TimeoutError
+				puts "tps depasse dans boucle infinie"
+			end #fin begin
+		end
+		#binding.pry
+	end
+
 
 end #fin de la class
 
-
-page_url = "https://www.facebook.com/groups/214718712053384/members"
+#fr en bogota:
+#page_url = "https://www.facebook.com/groups/214718712053384/members"
+page_url="https://www.facebook.com/groups/1565896396963524/members"
 bot=Facebook_scrap.new(page_url)
 
 bot.log_in_to_facebook()
 
+bot.fb_grp_scrap()
